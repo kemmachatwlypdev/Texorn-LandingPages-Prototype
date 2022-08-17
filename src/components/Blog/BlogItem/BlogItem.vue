02925f16d70cda3c2173060a21241c33cefa1f97
kemmachatwlypdev <kemmachat.wlyp.dev@gmail.com>
2022-08-17 23:05:27 +0700

    route BlogItem:id

--- src/components/Blog/BlogItem/BlogItem.vue
+++ src/components/Blog/BlogItem/BlogItem.vue
@@ -4,24 +4,37 @@
       <navigation :color="color" :flat="flat" />
     </div>
     <br /><br /><br /><br />
-    <v-row no-gutters justify="center" v-for="blog in blogsdata" :key="blog.id">
-      <v-col align="start" cols="10" >
+    <v-row no-gutters justify="center" v-bind="this.blogsdata[id]">
+      <v-col align="start" cols="10">
         <div>
+          <button @click="showindex">Press log</button>
           <h1>
-            {{blog.topics}}
+            <!-- {{this.blogsdata[id].topics}} -->
           </h1>
-         <router-link :to="'/blogsitem/' + blog.id">Test Link</router-link>
+          <!-- <router-link :to="'/blogsitem/' + blog.id">Test Link</router-link> -->
           <v-row>
             <v-col md="12">
-              <img v-bind:src="blog.imagesrc" alt="RPA" width="90%" style="float: left;" />
-             <a href="https://www.facebook.com/">
-                <v-btn style="color: black; box-shadow: none; background-color: white; position: fixed;"
-                  class="mt-8">
+              <img
+                src="./BlogItemIMG/Robotic_process_automation-1108451648.png"
+                alt="RPA"
+                width="90%"
+                style="float: left;"
+              />
+              <a href="https://www.facebook.com/">
+                <v-btn
+                  style="color: black; box-shadow: none; background-color: white; position: fixed;"
+                  class="mt-8"
+                >
                   <v-icon>mdi-facebook</v-icon>
-                </v-btn>
-              </a><br><br>
-              <v-btn style="color: black; box-shadow: none; background-color: white; position: fixed;" dark
-                v-bind="attrs" v-on="on" class="mt-8">
+                </v-btn> </a
+              ><br /><br />
+              <v-btn
+                style="color: black; box-shadow: none; background-color: white; position: fixed;"
+                dark
+                v-bind="attrs"
+                v-on="on"
+                class="mt-8"
+              >
                 <v-icon>mdi-twitter</v-icon>
               </v-btn>
             </v-col>
@@ -30,17 +43,20 @@
           <br />
           <br />
           <div class="text-align: center">
-            <article>
-              {{blog.article}}
-            </article>
+            <article></article>
           </div>
         </div>
         <div class="col-md-5" id="youtubeEmbed">
           <br />
-          <iframe width="100%" height="350" src="https://www.youtube.com/embed/9URSbTOE4YI" title="YouTube video player"
+          <iframe
+            width="100%"
+            height="350"
+            src="https://www.youtube.com/embed/9URSbTOE4YI"
+            title="YouTube video player"
             frameborder="0"
             allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
-            allowfullscreen></iframe>
+            allowfullscreen
+          ></iframe>
         </div>
       </v-col>
     </v-row>
@@ -50,6 +66,8 @@
 <script setup>
 import navigation from "../NavigationBlogs.vue";
 import blogs from "./dataBlogs/data.js";
+import axios from "axios";
+
 
 export default {
   components: {
@@ -57,12 +75,36 @@ export default {
   },
   data(){
     return{
-      blogsdata:blogs
+      blogsdata:[],
+      id:'',
     }
+  },
+  mounted(){
+    this.id=this.$route.params.id;
+    axios.get('src\components\Blog\BlogItem\dataBlogs\data.js').then((response) => {
+      this.blogsdata=response.data
+      console.log(response.data)
+    });
+  },
+  // watch:{
+  //   $route{
+  //     this.blogsdata.id = $route.params.id;
+  //   }
+  // },
+  methods:{
+    showindex(){
+      // const index=this.blogsdata.map(obj => obj.id)
+      // const topics=this.blogsdata.map(obj => obj.topics)
+      // const id=this.$route.params.id
+      console.log()
+      // console.log(this.blogsdata[index[0]].topics)
+    }
+    // const index=this.blogsdata.map(obj => obj.id)
+    // showconsole(){
+    //   console.log(this.blogsdata[1].topics);
+    // }
   }
 };
-
 </script>
 
-<style lang="scss" scoped>
-</style>
+<style lang="scss" scoped></style>
