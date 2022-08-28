7b1bdba44d9cc7cb7530598fd2ae5267360483f5
kemmachatwlypdev <kemmachat.wlyp.dev@gmail.com>
2022-08-28 11:15:27 +0700

    fixed footer Input 2440x1440

--- src/components/DemoApp/InputForms/InputForms.vue
+++ src/components/DemoApp/InputForms/InputForms.vue
@@ -113,93 +113,7 @@
         </v-col>
       </v-row>
     </v-container>
-    <!-- <v-row no-gutters align="center" justify="center">
-      <v-col align="start" cols="10">
-        <validation-observer ref="observer" v-slot="{ invalid }">
-          <form @submit.prevent="submit">
-            <validation-provider
-              v-slot="{ errors }"
-              name="Name"
-              rules="required|max:10"
-            >
-              <v-text-field
-                v-model="name"
-                :counter="10"
-                :error-messages="errors"
-                label="Name"
-                required
-              ></v-text-field>
-            </validation-provider>
-            <validation-provider
-              v-slot="{ errors }"
-              name="phoneNumber"
-              :rules="{
-                required: true,
-                digits: 7,
-                regex: '^(71|72|74|76|81|82|84|85|86|87|88|89)\\d{5}$',
-              }"
-            >
-              <v-text-field
-                v-model="phoneNumber"
-                :counter="7"
-                :error-messages="errors"
-                label="Phone Number"
-                required
-              ></v-text-field>
-            </validation-provider>
-            <validation-provider
-              v-slot="{ errors }"
-              name="email"
-              rules="required|email"
-            >
-              <v-text-field
-                v-model="email"
-                :error-messages="errors"
-                label="E-mail"
-                required
-              ></v-text-field>
-            </validation-provider>
-            <validation-provider
-              v-slot="{ errors }"
-              name="select"
-              rules="required"
-            >
-              <v-select
-                v-model="select"
-                :items="items"
-                :error-messages="errors"
-                label="Select"
-                data-vv-name="select"
-                required
-              ></v-select>
-            </validation-provider>
-            <validation-provider
-              v-slot="{ errors }"
-              rules="required"
-              name="checkbox"
-            >
-              <v-checkbox
-                v-model="checkbox"
-                :error-messages="errors"
-                value="1"
-                label="Option"
-                type="checkbox"
-                required
-              ></v-checkbox>
-            </validation-provider>
-
-            <v-btn class="mr-4" type="submit" :disabled="invalid">
-              submit
-            </v-btn>
-            <v-btn @click="clear">
-              clear
-            </v-btn>
-          </form>
-          <br /><br /><br />
-        </validation-observer>
-      </v-col>
-    </v-row> -->
-    <div><foote /></div>
+    <div id="footerInput"><foote /></div>
   </div>
 </template>
 
@@ -272,3 +186,11 @@ export default {
   },
 };
 </script>
+
+<style>
+#footerInput{
+            position: fixed;
+            bottom: 0;
+            width: 100%;
+}
+</style>
